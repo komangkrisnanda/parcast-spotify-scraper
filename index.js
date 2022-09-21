b3ba62be070a8e1d67a0305b4aa2b7f4b160b478
Komang Krisnanda <officialkrisnandha@gmail.com>
2022-09-21 18:05:27 +0800

    update: map zodiac & link

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-//
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const response = [];
+
+const zodiacList = ["Pisces", "Aries", "Virgo", "Scorpio"];
+
+const base_url = "https://open.spotify.com";
+
+const getEpisodes = async () => {
+	try {
+		const { data } = await axios.get(
+			`${base_url}/show/1UvmWHLiRH9IeTYfWC2zvc`
+		);
+		const $ = cheerio.load(data);
+		const episodeTitles = [];
+
+		$('div[data-testid^="episode-"]').each((_idx, el) => {
+			const episodeEl = $(el).text()
+            var episodeName = episodeEl.replace(/ .*/,'');
+            console.log(episodeName);
+            if(zodiacList.includes(episodeName)){
+                response.push({
+                    zodiac_name: episodeName,
+                    href: `${base_url}${$(el).find('a').attr('href')}`
+                })
+            }
+			
+		});
+
+		return episodeTitles;
+	} catch (error) {
+		throw error;
+	}
+};
+
+getEpisodes()
+    .then((episodeTitle) => console.log(response));
